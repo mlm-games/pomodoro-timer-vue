910f6d9f82d374d55e4545adc3833e6c60593961
mlm-games <125530737+mlm-games@users.noreply.github.com>
2024-12-02 22:35:17 +0530

    working electron build

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+  plugins: [vue()],
+  base: process.env.ELECTRON ? './' : '/',
+  server: {
+    port: 5173
+  }
 })
